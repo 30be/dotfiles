b8221386694170f9484863a2f8415ada3332153d
Lykd <lykd@pm.me>
2025-09-30 15:57:57 +0300

    Update sync.sh

--- executable_sync.sh
+++ executable_sync.sh
@@ -6,5 +6,5 @@ cd /home/lyka/books
 git add -A && git commit -m "Hourly auto-commit $(date +%%Y-%%m-%%d_%%H:%%M)" && git push
 cd /home/lyka/.task
 git add -A && git commit -m "Hourly auto-commit $(date +%%Y-%%m-%%d_%%H:%%M)" && git push
-yay -Qqe >~/pkglist.txt
-chezmoi add pkglist
+yay -Qqe >/home/lyka/pkglist.txt
+chezmoi add /home/lyka/pkglist.txt
