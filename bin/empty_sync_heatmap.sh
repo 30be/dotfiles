f75cf4fc1d078aa0d5b1bf3819c833cb5c48202a
Lykd <lykd@pm.me>
2025-10-11 15:40:54 +0300

    Update bin/sync_heatmap.sh

--- bin/empty_sync_heatmap.sh
+++
@@ -1,3 +0,0 @@
-rsync -avz --delete --no-o --no-g /home/lyka/heatmap.json root@23.94.5.170:/srv/shoggothstaring.com/ &&
-  ssh root@23.94.5.170 "chown -R caddy:caddy /srv/shoggothstaring.com/heatmap.json" &&
-  echo "Sync and ownership fixed." || echo "Sync failed."
