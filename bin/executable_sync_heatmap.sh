b14d3b29b7ed0554465451f85ca979072c935360
Lykd <lykd@pm.me>
2025-10-13 15:46:03 +0300

    Change attributes of bin/sync_heatmap.sh

--- bin/executable_sync_heatmap.sh
+++ bin/executable_sync_heatmap.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 rsync -avz --delete --no-o --no-g /home/lyka/heatmap.json root@23.94.5.170:/srv/shoggothstaring.com/ &&
   ssh root@23.94.5.170 "chown caddy:caddy /srv/shoggothstaring.com/heatmap.json" &&
   echo "Sync and ownership fixed." || echo "Sync failed."
