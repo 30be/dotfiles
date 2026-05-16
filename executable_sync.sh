29dac03e4b8a8d829bc7a7e57673bffefd971b1c
Lykd <lykd@pm.me>
2026-05-16 15:19:03 +0300

    Hourly auto-commit 2026-05-16_15:19

--- executable_sync.sh
+++ executable_sync.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 cd /home/lyka/d
-git add -A; git commit -m "Hourly auto-commit $(date +%Y-%m-%d_%H:%M)"; git push
+git add -A; git commit -m "Hourly auto-commit $(date +%Y-%m-%d_%H:%M)"; git pull --rebase; git push
 gpg --batch --yes --trust-model always --encrypt --recipient 042F2E611F54049F --output /home/lyka/.password-store/env.gpg /home/lyka/.env
 cd /home/lyka/.password-store
-git add -A; git commit -m "Hourly auto-commit $(date +%Y-%m-%d_%H:%M)"; git push
+git add -A; git commit -m "Hourly auto-commit $(date +%Y-%m-%d_%H:%M)"; git pull --rebase; git push
 
 cd /home/lyka/.local/share/chezmoi
 chezmoi re-add
-git add -A; git commit -m "Hourly auto-commit $(date +%Y-%m-%d_%H:%M)"; git push
+git add -A; git commit -m "Hourly auto-commit $(date +%Y-%m-%d_%H:%M)"; git pull --rebase; git push
 
 cd /home/lyka/dev/30be.github.io/
 cp /home/lyka/heatmap.json static/
 cabal run . build
-git add -A; git commit -m "Hourly auto-commit $(date +%Y-%m-%d_%H:%M)"; git push
+git add -A; git commit -m "Hourly auto-commit $(date +%Y-%m-%d_%H:%M)"; git pull --rebase; git push
