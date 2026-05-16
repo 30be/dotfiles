9104733b15e1260e2f3b4cd7c8cd7773bc939d4c
Lykd <lykd@pm.me>
2026-05-16 15:21:12 +0300

    Hourly auto-commit 2026-05-16_15:21

--- executable_sync.sh
+++ executable_sync.sh
@@ -2,7 +2,9 @@
 
 cd /home/lyka/d
 git add -A; git commit -m "Hourly auto-commit $(date +%Y-%m-%d_%H:%M)"; git pull --rebase; git push
-gpg --batch --yes --trust-model always --encrypt --recipient 042F2E611F54049F --output /home/lyka/.password-store/env.gpg /home/lyka/.env
+if ! gpg --batch --quiet --decrypt /home/lyka/.password-store/env.gpg 2>/dev/null | diff -q - /home/lyka/.env >/dev/null 2>&1; then
+  gpg --batch --yes --trust-model always --encrypt --recipient 042F2E611F54049F --output /home/lyka/.password-store/env.gpg /home/lyka/.env
+fi
 cd /home/lyka/.password-store
 git add -A; git commit -m "Hourly auto-commit $(date +%Y-%m-%d_%H:%M)"; git pull --rebase; git push
 
